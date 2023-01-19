661cb7bf190f0da30cb2ec06bdb9ea946a799c5c
b1i <111800770+b1i@users.noreply.github.com>
2023-01-19 05:17:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-function freaky(a, b) {
-  function redirect(url, callback) {
-    fetch(url, { method: "HEAD" }).then(function (status) {
-      callback(status.ok)
-    })
+const freaky = (function() {
+  function checkLink(url, redirectUrl) {
+    fetch(url, { method: 'HEAD' })
+      .then(response => {
+        if (response.status === 404) {
+          // link leads to a 404 page
+          window.location.href = redirectUrl;
+        }
+      })
+      .catch(() => {
+        // handle any errors that may occur during the fetch
+      });
   }
 
-  redirect(a, function (exists) {
-    if (exists) {
-      window.location.replace(a)
-    } else {
-      window.location.replace(b)
-    }
-  })
-}
+  function checkLinkOnBeforeUnload(url, redirectUrl) {
+    window.addEventListener("beforeunload", e => {
+        checkLink(url, redirectUrl);
+    });
+  }
 
-
-
+  function checkLinkOnPopState(url, redirectUrl) {
+    window.addEventListener("popstate", e => {
+        checkLink(url, redirectUrl);
+    });
+  }
+  return {
+    checkLink,
+    checkLinkOnBeforeUnload,
+    checkLinkOnPopState
+  };
+})();
